97b671a3da71a76f16fec7bd04fe1fb3dac378fb
Leonardo1623 <leonardo.ab92@gmail.com>
2015-11-24 21:58:44 -0700

    Comentario
    Cambio de comentario

--- Proyecto/HotSoft/HotSoft/IniciarPerfil.aspx.cs
+++ Proyecto/HotSoft/HotSoft/IniciarPerfil.aspx.cs
@@ -12,7 +12,7 @@ namespace HotSoft
 {
     public partial class IniciarPerfil : System.Web.UI.Page
     {
-		//CHSM EL TRINI 
+		//COMENTARIO BONITO
         protected void Page_Load(object sender, EventArgs e)
         {
 
